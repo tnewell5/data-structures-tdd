da262abeac1f8342d0ead56cf20f15fa5a4f0086
Tanya Newell <trozova5@gmail.com>
2016-07-15 14:50:46 -0400

    got both tests to pass

--- linked-lists.js
+++ linked-lists.js
@@ -1,13 +1,41 @@
-function ListNode(val) {
-  this.val = val;
-  this.next = null;
-}
+var Lists;
+
+Lists = {
+  listNode: function(val) {
+    this.val = val;
+    this.next = null;
+  },
+  //delete a node from the list:
+  // iterate over the list and keep track of previous value
+  // once node value is a match, set the prevoius.next to current.next
+  deleteNode: function(nodeVal) {
+    var prev = myList;
+    var pointer = myList;
+    // if this is the first node in the list, set myList to myList.next and myList.previous to null
+    if (pointer.val === nodeVal) {
+      myList = myList.next;
+      myList.previous = null;
+    }
+    // while pointing to a valid node:
+    while (pointer) {
+      if (pointer.val === nodeVal) {
+        prev.next = pointer.next;
+        if (pointer.next) {
+        pointer.next.previous = prev;
+        }
+      }
+      prev = pointer;
+      pointer = pointer.next;
+    }
+  }
+};
+
+console.log(new Lists.listNode(25));
 
 // create new singly-linked list:
-var myList = new ListNode(1);
-myList.next = new ListNode(2);
-myList.next.next = new ListNode(3);
-
+var myList = new Lists.listNode(1);
+myList.next = new Lists.listNode(2);
+myList.next.next = new Lists.listNode(3);
 var head = myList;
 
 // create a doubly-linked list:
@@ -31,8 +59,8 @@ while (pointer1 && pointer2) {
   pointer2 = pointer2.previous;
 }
 
-//first create new ListNode:
-var newNode = new ListNode(5);
+//first create new listNode:
+var newNode = new Lists.listNode(5);
 newNode.previous = null;
 
 //now connect newNode to the rest of the list:
@@ -40,29 +68,8 @@ newNode.next = myList;
 myList.previous = newNode;
 myList = myList.previous;
 
-//delete a node from the list:
-// iterate over the list and keep track of previous value
-// once node value is a match, set the prevoius.next to current.next
-function deleteNode(nodeVal) {
-  var prev = myList;
-  var pointer = myList;
-  // if this is the first node in the list, set myList to myList.next and myList.previous to null
-  if (pointer.val === nodeVal) {
-    myList = myList.next;
-    myList.previous = null;
-  }
-  // while pointing to a valid node:
-  while (pointer) {
-    if (pointer.val === nodeVal) {
-      prev.next = pointer.next;
-      if (pointer.next) {
-      pointer.next.previous = prev;
-      }
-    }
-    prev = pointer;
-    pointer = pointer.next;
-  }
-}
+Lists.deleteNode(3);
+//console.log('myList: ', myList);
 
-deleteNode(3);
-console.log('myList: ', myList);
+
+module.exports = Lists;
--- tests-spec.js
+++ tests-spec.js
@@ -1,9 +1,17 @@
 'use strict';
 var expect = require('chai').expect;
-var LinkedLists = require('./linked-lists.js');
+var Lists = require('./linked-lists.js');
 
-describe('LinkedLists', function() {
+describe('Lists', function() {
   it('should exist', function() {
-    expect(LinkedLists).to.not.be.undefined;
+    expect(Lists).to.not.be.undefined;
+  });
+});
+
+describe('#listNode()', function() {
+  it('should take in val and create a linked list node with val as value and point to null', function() {
+    var expected = { val: 25, next: null };
+    var actual = new Lists.listNode(25);
+    expect(actual).to.eql(expected);
   });
 });
